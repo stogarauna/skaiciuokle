e7c261fe617d79d345b3ec67adacf2b8d2d76495
Gulinciu Nemusa <gulinciu_nemusa@Gulincius-MacBook-Air-2.local>
2025-10-05 22:51:41 +0300

    Pirmas commit: pridedu Skaiciuoklę

--- src/lib/data/panels.js
+++ src/lib/data/panels.js
@@ -1,6 +1,6 @@
 export const panels = [
 	{
-		name: "P2.6 Indoor",
+		name: "P2 XR",
 		resX: 192,
 		resY: 192,
 		widthM: 0.5,
@@ -9,12 +9,30 @@ export const panels = [
 		weightKg: 7.5
 	},
 	{
-		name: "P3.9 Outdoor",
+		name: "P2 YESTECH",
+		resX: 192,
+		resY: 192,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 180,
+		weightKg: 7.5
+	},
+	{
+		name: "P3 INFI G1",
+		resX: 128,
+		resY: 256,
+		widthM: 0.5,
+		heightM: 1.0,
+		power: 340,
+		weightKg: 8.2
+	},
+	{
+		name: "P3 INFI G2",
 		resX: 128,
 		resY: 128,
 		widthM: 0.5,
 		heightM: 0.5,
-		power: 300,
+		power: 170,
 		weightKg: 8.2
 	},
 	{
