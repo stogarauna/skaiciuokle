1d8c03edec7f1f9d6a6d31f108a71bd7d76fe87d
stogarauna <stoga.rauna@gmail.com>
2025-10-06 17:08:27 +0300

    Pašalintas viršutinis užrašas Skaičiuoklė

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
-<header class="bg-gradient-to-r from-blue-600 to-indigo-600 text-white text-center py-4 font-semibold shadow-md">
-	Skaičiuoklė
-</header>
+
 
 <main class="min-h-screen bg-gray-100 flex flex-col items-center px-4 py-6" style="font-size: 1.8rem !important; line-height: 1 !important;">
 	<div class="w-full max-w-md bg-white rounded-2xl shadow-lg p-5">
