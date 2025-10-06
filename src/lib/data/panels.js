aedf0563a9d6d8376bb8404121010912d5c9e8d9
stogarauna <stoga.rauna@gmail.com>
2025-10-06 13:12:51 +0300

    Atnaujinta LED modelių biblioteka

--- src/lib/data/panels.js
+++ src/lib/data/panels.js
@@ -1,12 +1,30 @@
 export const panels = [
 	{
+		name: "P1 LEDTITGO",
+		resX: 256,
+		resY: 256,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 170,
+		weightKg: 12.5
+	},
+	{
+		name: "P2 Curved",
+		resX: 192,
+		resY: 192,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 150,
+		weightKg: 6.3
+	},
+	{
 		name: "P2 XR",
 		resX: 192,
 		resY: 192,
 		widthM: 0.5,
 		heightM: 0.5,
-		power: 180,
-		weightKg: 7.5
+		power: 150,
+		weightKg: 6.3
 	},
 	{
 		name: "P2 YESTECH",
@@ -14,8 +32,8 @@ export const panels = [
 		resY: 192,
 		widthM: 0.5,
 		heightM: 0.5,
-		power: 180,
-		weightKg: 7.5
+		power: 145,
+		weightKg: 6.7
 	},
 	{
 		name: "P3 INFI G1",
@@ -24,7 +42,7 @@ export const panels = [
 		widthM: 0.5,
 		heightM: 1.0,
 		power: 340,
-		weightKg: 8.2
+		weightKg: 13.4
 	},
 	{
 		name: "P3 INFI G2",
@@ -33,33 +51,87 @@ export const panels = [
 		widthM: 0.5,
 		heightM: 0.5,
 		power: 170,
-		weightKg: 8.2
+		weightKg: 8.1
 	},
 	{
-		name: "P4.8 Outdoor (500×1000 mm)",
-		resX: 104,
-		resY: 208,
+		name: "P3 (500×500)",
+		resX: 128,
+		resY: 128,
 		widthM: 0.5,
-		heightM: 1.0,
-		power: 350,
-		weightKg: 8.5
+		heightM: 0.5,
+		power: 200,
+		weightKg: 10.0
 	},
 	{
-		name: "P5.9 Outdoor (500×1000 mm)",
-		resX: 84,
-		resY: 168,
+		name: "P3 (500×1000)",
+		resX: 128,
+		resY: 256,
 		widthM: 0.5,
 		heightM: 1.0,
-		power: 400,
+		power: 300,
+		weightKg: 14.0
+	},
+	{
+		name: "P3 Spectrum",
+		resX: 128,
+		resY: 128,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 235,
 		weightKg: 9.0
 	},
 	{
-		name: "P1.9 Indoor HD",
-		resX: 256,
-		resY: 256,
+		name: "P4 YESTECH",
+		resX: 104,
+		resY: 104,
 		widthM: 0.5,
 		heightM: 0.5,
-		power: 160,
+		power: 150,
+		weightKg: 6.5
+	},
+	{
+		name: "P4 Spectrum",
+		resX: 104,
+		resY: 104,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 235,
+		weightKg: 9.0
+	},
+	{
+		name: "P5 YESTECH",
+		resX: 84,
+		resY: 84,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 210,
 		weightKg: 7.0
+	},
+	{
+		name: "P7 ROE",
+		resX: 80,
+		resY: 80,
+		widthM: 0.6,
+		heightM: 0.6,
+		power: 200,
+		weightKg: 7.95
+	},
+	{
+		name: "Transparent(1000x500)",
+		resX: 256,
+		resY: 64,
+		widthM: 1.0,
+		heightM: 0.5,
+		power: 210,
+		weightKg: 6.5
+	},
+	{
+		name: "Transparent(500x500)",
+		resX: 128,
+		resY: 64,
+		widthM: 0.5,
+		heightM: 0.5,
+		power: 100,
+		weightKg: 4.6
 	}
 ];
