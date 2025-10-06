325a4d9b4496ffa9b9ac3a372f2592ae94a46ae0
stogarauna <stoga.rauna@gmail.com>
2025-10-06 16:51:25 +0300

    Sumažintas tekstas 30% ant main elemento

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	Skaičiuoklė
 </header>
 
-<main class="min-h-screen bg-gray-100 flex flex-col items-center px-4 py-6" style="font-size: 2rem !important; line-height: 1.1 !important;">
+<main class="min-h-screen bg-gray-100 flex flex-col items-center px-4 py-6" style="font-size: 1.4rem !important; line-height: 1 !important;">
 	<div class="w-full max-w-md bg-white rounded-2xl shadow-lg p-5">
 		<slot />
 	</div>
